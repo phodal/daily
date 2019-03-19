cccf09408397786f3a8bfb3aab218fbb35bffc8e
Phodal HUANG <h@phodal.com>
2019-03-19 10:23:00 +0800

    fix: add basci thumbnail

---
+++ thumbnail.py
@@ -0,0 +1,22 @@
+# from PIL import Image
+# import glob, os
+# 
+# size = 128, 128
+# 
+# for infile in glob.glob("*.jpg"):
+#     file, ext = os.path.splitext(infile)
+#     im = Image.open(infile)
+#     im.thumbnail(size)
+#     im.save(file + ".thumbnail", "JPEG")
+
+
+import os
+import subprocess
+
+for root, dirs, files in os.walk('images'):
+  images = [os.path.join(root, f) for f in files if f.endswith('.jpg') and not '.thumbnail' in f]
+  for f in images:
+    outbase = f[:-4]  # simply remove '.jpg'
+    out = outbase = outbase +'.thumbnail.jpg'
+    args = ['convert', f, '-scale', '250x250', out]
+    subprocess.call(args)
